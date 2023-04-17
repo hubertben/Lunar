fbf1b3385a765d74cc84192970c7b03726bc9cf2
Ben <hubertben365@gmail.com>
2023-04-17 15:21:40 -0400

    setup

---
+++ LC/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace LC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            keyboardInput();
+        }
+
+        static void keyboardInput()
+        {
+            while (true)
+            {
+
+                var key = Console.ReadLine();
+
+                if (key == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(key);
+                }
+
+            }
+        }
+
+    }
+}
